3cc146013bf1195a8cc7e34fbc4093b49c9c0584
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-20 18:58:31 -0300

    846-ASPNETCore20-video4.5.Obtendo ou Criando Novo Pedido

--- Aulas/Aula3/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula3/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -9,7 +9,7 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IPedidoRepository
     {
-
+        Pedido GetPedido();
     }
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
@@ -20,6 +20,23 @@ namespace CasaDoCodigo.Repositories
             IHttpContextAccessor contextAccessor) : base(contexto)
         {
             this.contextAccessor = contextAccessor;
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet;
+                .Where(p => p.Id == pedidoId)
+                .SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                dbSet.Add(pedido);
+                contexto.SaveChanges();
+            }
+
+            return pedido;
         }
 
         private int? GetPedidoId()
