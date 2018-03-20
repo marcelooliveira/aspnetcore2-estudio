fd67acaf26ed37129c232eef91dc1d5d70d1556f
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-20 20:27:32 -0300

    846-ASPNETCore20-video4.6.Salvando Item de Pedido

--- Aulas/Aula3/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula3/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -10,6 +10,7 @@ namespace CasaDoCodigo.Repositories
     public interface IPedidoRepository
     {
         Pedido GetPedido();
+        void AddItem(string codigo);
     }
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
@@ -20,6 +21,34 @@ namespace CasaDoCodigo.Repositories
             IHttpContextAccessor contextAccessor) : base(contexto)
         {
             this.contextAccessor = contextAccessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>()
+                            .Where(p => p.Codigo == codigo)
+                            .SingleOrDefault();
+
+            if (produto == null)
+            {
+                throw new ArgumentException("Produto não encontrado");
+            }
+
+            var pedido = GetPedido();
+
+            var itemPedido = contexto.Set<ItemPedido>()
+                                .Where(i => i.Produto.Codigo == codigo
+                                        && i.Pedido.Id == pedido.Id)
+                                .SingleOrDefault();
+
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>()
+                    .Add(itemPedido);
+
+                contexto.SaveChanges();
+            }
         }
 
         public Pedido GetPedido()
@@ -34,6 +63,7 @@ namespace CasaDoCodigo.Repositories
                 pedido = new Pedido();
                 dbSet.Add(pedido);
                 contexto.SaveChanges();
+                SetPedidoId(pedido.Id);
             }
 
             return pedido;
